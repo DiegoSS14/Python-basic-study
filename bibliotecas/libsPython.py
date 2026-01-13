f3308df72a8baf4c788cc3c2a7bf252c72f5e9a0
DiegoSS14 <sousadiego533@gmail.com>
2026-01-13 15:56:51 -0300

    Instalacao de libs externas

---
+++ bibliotecas/libsPython.py
@@ -0,0 +1,13 @@
+import math
+import random
+import numpy as np
+
+nota = 6.5666
+
+print(round(nota, 2))
+print(math.floor(nota)) # arredonda pra baixo
+print(math.ceil(nota)) # arredonda pra cima
+print(math.pi) # Exibe o valor de PI
+
+print(round(random.random() * 100)) # Gera um número aleatório de 0 a 100
+print(random.randint(1, 100)) # Gera um número aleatório de 0 a 100
